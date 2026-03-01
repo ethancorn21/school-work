79a7e0709d6eae6f733bee29059be4c221074e40
Ethan Corn <ethan@Ethans-MacBook-Pro.local>
2026-02-28 21:32:33 -0600

    9.3B, sort within an array of objects

--- INFS3400/09_06_B/SaleTaxDemo2.cs
+++ INFS3400/09_06_B/SaleTaxDemo2.cs
@@ -15,10 +15,14 @@ class SalesTaxDemo
 		//tax rate = 8% for first $100
 		//6% tax rate for any amount more than $100
 		//if (amount > 100){salestax = .06};else{salextax=.08};
+
+		//after 10 displayed, sort by highest sales tax then display again
+		//modify Modify the Sale class so its objects are comparable to each other based on tax owed
+		//using IComparable.CompareTo()
 		Sale[] arr = new Sale[10];
 		for (int i = 0; i < 10; ++i)
 		{
-			Console.Write("Enter inventory number #{0} >> ", i+1);
+			Console.Write("Enter inventory number #{0} >> ", i + 1);
 			string inventoryNum = ReadLine();
 			Console.Write("Enter amount of sale >> ");
 			double saleAmount = Double.Parse(ReadLine());
@@ -29,23 +33,51 @@ class SalesTaxDemo
 				Amount = saleAmount,
 			};
 		}
-		for(int i = 0; i < 10; ++i)
+
+		for (int i = 0; i < 10; ++i)
 		{
-			Console.WriteLine("Sale # {0} Amount: {1} Sale {2}", i+1,arr[i].InventoryNumber, arr[i].Amount.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+			Console.WriteLine("Sale # {0} Amount: {1} Sale {2}", i + 1, arr[i].InventoryNumber, arr[i].Amount.ToString("C", CultureInfo.GetCultureInfo("en-US")));
 			Console.WriteLine("     Tax is {0}", arr[i].Tax.ToString("C", CultureInfo.GetCultureInfo("en-US")));
 		}
-	}
+
+		Array.Sort(arr);
+
+		for (int i = 0; i < 10; ++i)
+		{
+			Console.WriteLine("Sale # {0} Amount: {1} Sale {2}", i + 1, arr[i].InventoryNumber, arr[i].Amount.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+			Console.WriteLine("     Tax is {0}", arr[i].Tax.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+		}
+	}	
 }
 
-public class Sale
+
+public class Sale : IComparable
 {
-	public string InventoryNumber{get;set;}
-	public double Amount{get;set;}
-	public double Tax{
-		get{
-			if(Amount>100){return ((Amount-100)*.06) + 8.00;}
-			else{return Amount*.08;}
+	public string InventoryNumber { get; set; }
+	public double Amount { get; set; }
+	public double Tax
+	{
+		get
+		{
+			if (Amount > 100) { return ((Amount - 100) * .06) + 8.00; }
+			else { return Amount * .08; }
 		}
 	}
 
+    int IComparable.CompareTo(Object other)
+	{
+		Sale temp = (Sale)other;
+		int returnVal;
+
+		if (this.Tax < temp.Tax) returnVal = 1;
+		else
+		{
+			if (this.Tax > temp.Tax) returnVal = -1;
+			else
+			{
+				returnVal = 0;
+			}
+		}
+		return returnVal;
+    }
 }
